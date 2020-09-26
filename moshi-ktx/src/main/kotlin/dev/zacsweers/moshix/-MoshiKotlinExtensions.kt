dc6f4009be33a9db9466addcd3d1d842ce6a1e5f
Zac Sweers <pandanomic@gmail.com>
2020-09-26 05:03:16 -0400

    Use Kotlin 1.4's javaType API

--- moshi-ktx/src/main/kotlin/dev/zacsweers/moshix/-MoshiKotlinExtensions.kt
+++ moshi-ktx/src/main/kotlin/dev/zacsweers/moshix/-MoshiKotlinExtensions.kt
@@ -17,15 +17,10 @@ package dev.zacsweers.moshix
 
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import com.squareup.moshi.internal.NonNullJsonAdapter
 import com.squareup.moshi.internal.NullSafeJsonAdapter
-import java.lang.reflect.Type
-import kotlin.reflect.KClass
 import kotlin.reflect.KType
-import kotlin.reflect.KTypeParameter
-import kotlin.reflect.KTypeProjection
-import kotlin.reflect.KVariance
+import kotlin.reflect.javaType
 import kotlin.reflect.typeOf
 
 /**
@@ -36,14 +31,15 @@ import kotlin.reflect.typeOf
 public inline fun <reified T> Moshi.adapter(): JsonAdapter<T> = adapter(typeOf<T>())
 
 @ExperimentalStdlibApi
-public inline fun <reified T> Moshi.Builder.addAdapter(adapter: JsonAdapter<T>): Moshi.Builder = add(typeOf<T>().toType(), adapter)
+public inline fun <reified T> Moshi.Builder.addAdapter(adapter: JsonAdapter<T>): Moshi.Builder = add(typeOf<T>().javaType, adapter)
 
 /**
  * @return a [JsonAdapter] for [ktype], creating it if necessary. Note that while nullability of
  *         [ktype] itself is handled, nested types (such as in generics) are not resolved.
  */
+@ExperimentalStdlibApi
 public fun <T> Moshi.adapter(ktype: KType): JsonAdapter<T> {
-  val adapter = adapter<T>(ktype.toType())
+  val adapter = adapter<T>(ktype.javaType)
   return if (adapter is NullSafeJsonAdapter || adapter is NonNullJsonAdapter) {
     // TODO CR - Assume that these know what they're doing? Or should we defensively avoid wrapping for matching nullability?
     adapter
@@ -51,56 +47,5 @@ public fun <T> Moshi.adapter(ktype: KType): JsonAdapter<T> {
     adapter.nullSafe()
   } else {
     adapter.nonNull()
-  }
-}
-
-@PublishedApi
-internal fun KType.toType(allowPrimitives: Boolean = true): Type {
-  classifier?.let {
-    when (it) {
-      is KTypeParameter -> throw IllegalArgumentException("Type parameters are not supported")
-      is KClass<*> -> {
-        val javaType = if (allowPrimitives) {
-          it.java
-        } else {
-          it.javaObjectType
-        }
-        if (javaType.isArray) {
-          return Types.arrayOf(javaType.componentType)
-        }
-
-        return if (arguments.isEmpty()) {
-          javaType
-        } else {
-          val typeArguments = arguments.toTypedArray(KTypeProjection::toType)
-          val enclosingClass = javaType.enclosingClass
-          return if (enclosingClass != null) {
-            Types.newParameterizedTypeWithOwner(enclosingClass, javaType, *typeArguments)
-          } else {
-            Types.newParameterizedType(javaType, *typeArguments)
-          }
-        }
-      }
-      else -> throw IllegalArgumentException("Unsupported classifier: $this")
-    }
-  }
-
-  // Can happen for intersection types
-  throw IllegalArgumentException("Unrepresentable type: $this")
-}
-
-internal fun KTypeProjection.toType(): Type {
-  val javaType = type?.toType(allowPrimitives = false) ?: return Any::class.java
-  return when (variance) {
-    null -> Any::class.java
-    KVariance.INVARIANT -> javaType
-    KVariance.IN -> Types.subtypeOf(javaType)
-    KVariance.OUT -> Types.supertypeOf(javaType)
-  }
-}
-
-private inline fun <T, reified R> List<T>.toTypedArray(mapper: (T) -> R): Array<R> {
-  return Array(size) {
-    mapper.invoke(get(it))
   }
 }
