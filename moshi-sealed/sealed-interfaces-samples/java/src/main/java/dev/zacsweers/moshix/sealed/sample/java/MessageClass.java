8cffd8afc62b943582f7a252ab8f4abe3f929f77
Zac Sweers <pandanomic@gmail.com>
2021-01-26 16:15:03 -0500

    Move tests to samples as integration so junit is happy

--- moshi-sealed/sealed-interfaces-samples/java/src/main/java/dev/zacsweers/moshix/sealed/sample/java/MessageClass.java
+++ moshi-sealed/sealed-interfaces-samples/java/src/main/java/dev/zacsweers/moshix/sealed/sample/java/MessageClass.java
@@ -3,12 +3,13 @@ package dev.zacsweers.moshix.sealed.sample.java;
 import com.squareup.moshi.JsonClass;
 
 import java.util.Map;
+import java.util.Objects;
 
 import dev.zacsweers.moshix.sealed.annotations.TypeLabel;
 
 // @DefaultObject is not possible in java
 @JsonClass(generateAdapter = true, generator = "sealed:type")
-public sealed class MessageClass permits MessageClass.Success, MessageClass.Error {
+sealed class MessageClass permits MessageClass.Success, MessageClass.Error {
 
   @TypeLabel(label = "success", alternateLabels = {"successful"})
   static final class Success extends MessageClass {
@@ -17,6 +18,21 @@ public sealed class MessageClass permits MessageClass.Success, MessageClass.Erro
 
     Success(String value) {
       this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) { return true; }
+      if (o == null || getClass() != o.getClass()) { return false; }
+
+      Success success = (Success) o;
+
+      return Objects.equals(value, success.value);
+    }
+
+    @Override
+    public int hashCode() {
+      return value != null ? value.hashCode() : 0;
     }
   }
 
@@ -27,6 +43,21 @@ public sealed class MessageClass permits MessageClass.Success, MessageClass.Erro
 
     Error(Map<String, Object> error_logs) {
       this.error_logs = error_logs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) { return true; }
+      if (o == null || getClass() != o.getClass()) { return false; }
+
+      Error error = (Error) o;
+
+      return Objects.equals(error_logs, error.error_logs);
+    }
+
+    @Override
+    public int hashCode() {
+      return error_logs != null ? error_logs.hashCode() : 0;
     }
   }
 }
--- moshi-sealed/sealed-interfaces-samples/java/src/main/java/dev/zacsweers/moshix/sealed/sample/java/MessageWithNullDefault.java
+++ moshi-sealed/sealed-interfaces-samples/java/src/main/java/dev/zacsweers/moshix/sealed/sample/java/MessageWithNullDefault.java
@@ -4,18 +4,21 @@ import com.squareup.moshi.JsonClass;
 
 import java.util.Map;
 
+import dev.zacsweers.moshix.sealed.annotations.DefaultNull;
 import dev.zacsweers.moshix.sealed.annotations.TypeLabel;
 
 // @DefaultObject is not possible in java
+@DefaultNull
 @JsonClass(generateAdapter = true, generator = "sealed:type")
-sealed interface MessageInterface
-    permits MessageInterface.Success, MessageInterface.Error {
+sealed interface MessageWithNullDefault
+    permits MessageWithNullDefault.Success, MessageWithNullDefault.Error {
 
   @TypeLabel(label = "success", alternateLabels = {"successful"})
-  final record Success(String value) implements MessageInterface {
+  final record Success(String value) implements MessageWithNullDefault {
   }
 
   @TypeLabel(label = "error")
-  final record Error(Map<String, Object> error_logs) implements MessageInterface {
+  final record Error(Map<String, Object> error_logs) implements
+      MessageWithNullDefault {
   }
 }
