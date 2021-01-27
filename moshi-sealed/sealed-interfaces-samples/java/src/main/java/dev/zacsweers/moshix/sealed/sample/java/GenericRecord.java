f3a6ad81f4ab096b47c41cadeadd430346d3cc36
Zac Sweers <pandanomic@gmail.com>
2021-01-26 23:16:03 -0500

    Support generic records

---
+++ moshi-sealed/sealed-interfaces-samples/java/src/main/java/dev/zacsweers/moshix/sealed/sample/java/GenericRecord.java
@@ -0,0 +1,4 @@
+package dev.zacsweers.moshix.sealed.sample.java;
+
+public record GenericRecord<T>(T value) {
+}
---
+++ moshi-sealed/sealed-interfaces-samples/java/src/test/java/dev/zacsweers/moshix/sealed/sample/java/RecordJsonAdapterFactoryTest.java
@@ -0,0 +1,27 @@
+package dev.zacsweers.moshix.sealed.sample.java;
+
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import dev.zacsweers.moshix.records.RecordsJsonAdapterFactory;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public final class RecordJsonAdapterFactoryTest {
+
+  private final Moshi moshi = new Moshi.Builder()
+      .add(new RecordsJsonAdapterFactory())
+      .build();
+
+  @Test
+  public void genericRecord() throws IOException {
+    var adapter = moshi.<GenericRecord<String>>adapter(Types.newParameterizedType(GenericRecord.class, String.class));
+    assertThat(adapter.fromJson("{\"value\":\"Okay!\"}"))
+        .isEqualTo(new GenericRecord<>("Okay!"));
+  }
+
+}
