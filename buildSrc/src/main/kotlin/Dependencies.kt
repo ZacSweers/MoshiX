30b9ffbdd4c325ff9673666da39fc48a3b586795
Zac Sweers <pandanomic@gmail.com>
2021-05-12 20:16:34 -0400

    Update KSP

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -42,7 +42,7 @@ object Dependencies {
     val defaultFreeCompilerArgs = listOf("-Xjsr305=strict", "-progressive")
 
     object Ksp {
-      const val version = "1.5.0-1.0.0-alpha09"
+      const val version = "1.5.0-1.0.0-alpha10"
       const val api = "com.google.devtools.ksp:symbol-processing-api:$version"
       const val ksp = "com.google.devtools.ksp:symbol-processing:$version"
     }
