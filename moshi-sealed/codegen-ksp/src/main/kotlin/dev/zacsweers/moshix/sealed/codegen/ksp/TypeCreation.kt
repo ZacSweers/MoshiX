d87f29130b18f9c0379bd026bfd125c0730e5053
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-08-08 16:40:44 +0200

    add OptIn annotation when object adapters are used (#140)

--- moshi-sealed/codegen-ksp/src/main/kotlin/dev/zacsweers/moshix/sealed/codegen/ksp/TypeCreation.kt
+++ moshi-sealed/codegen-ksp/src/main/kotlin/dev/zacsweers/moshix/sealed/codegen/ksp/TypeCreation.kt
@@ -139,6 +139,15 @@ internal fun createType(
         .addMember("%S", "UNCHECKED_CAST")
         .build()
     )
+    .apply {
+      if (objectAdapters.isNotEmpty()) {
+        addAnnotation(
+          AnnotationSpec.builder(ClassName("kotlin", "OptIn"))
+            .addMember("%T::class", ClassName("kotlin", "ExperimentalStdlibApi"))
+            .build()
+        )
+      }
+    }
     .initializer(runtimeAdapterInitializer.build())
     .build()
 
