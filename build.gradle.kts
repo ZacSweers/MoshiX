c120f2e4bab6cf265d47e1b383afc182af74e72e
Zac Sweers <pandanomic@gmail.com>
2021-08-26 23:45:52 -0400

    Move daemon config to gradle property

--- build.gradle.kts
+++ build.gradle.kts
@@ -137,21 +137,6 @@ subprojects {
     if (project.name != "sample" && !project.path.contains("sample") && !project.path.contains("test")) {
       configure<KotlinProjectExtension> {
         explicitApi()
-        kotlinDaemonJvmArgs = listOf(
-          "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-          "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
-          "--add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
-          "--add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
-          "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED ",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
-          "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-        )
       }
     }
   }
