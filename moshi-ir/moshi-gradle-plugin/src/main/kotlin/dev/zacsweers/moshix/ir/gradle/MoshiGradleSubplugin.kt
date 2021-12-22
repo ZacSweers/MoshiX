4b19809e0b77f99866cf865547dada62a726e000
Zac Sweers <pandanomic@gmail.com>
2021-12-22 17:08:26 -0500

    Add moshi dependency in the gradle plugin (#188)
    * Add moshi dependency in the gradle plugin
    
    * Fix version

--- moshi-ir/moshi-gradle-plugin/src/main/kotlin/dev/zacsweers/moshix/ir/gradle/MoshiGradleSubplugin.kt
+++ moshi-ir/moshi-gradle-plugin/src/main/kotlin/dev/zacsweers/moshix/ir/gradle/MoshiGradleSubplugin.kt
@@ -44,6 +44,9 @@ class MoshiGradleSubplugin : KotlinCompilerPluginSupportPlugin {
     val extension = project.extensions.getByType(MoshiPluginExtension::class.java)
     val generatedAnnotation = extension.generatedAnnotation.orNull
 
+    // Minimum Moshi version
+    project.dependencies.add("api", "com.squareup.moshi:moshi:1.13.0")
+
     return project.provider {
       buildList {
         add(SubpluginOption(key = "enabled", value = extension.enabled.get().toString()))
