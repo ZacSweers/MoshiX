97e92dc8e36073e2eb7caeeabd97060a8012880c
Zac Sweers <pandanomic@gmail.com>
2022-05-27 18:24:45 -0400

    Remove leftover debugging test

--- moshi-ir/moshi-compiler-plugin/src/test/kotlin/dev/zacsweers/moshix/ir/compiler/MoshiIrVisitorTest.kt
+++ moshi-ir/moshi-compiler-plugin/src/test/kotlin/dev/zacsweers/moshix/ir/compiler/MoshiIrVisitorTest.kt
@@ -45,33 +45,6 @@ class MoshiIrVisitorTest {
   }
 
   @Test
-  fun exampleReflexive() {
-    val result =
-      compile(
-        kotlin(
-          "source.kt",
-          """
-          package test
-
-          import com.squareup.moshi.Json
-          import com.squareup.moshi.JsonClass
-          import com.squareup.moshi.JsonQualifier
-
-          sealed class Foo
-          class Subtype : Foo()
-
-          fun doStuff() {
-            Foo::class.sealedSubclasses
-          }
-          """
-        )
-      )
-    assertThat(result.exitCode).isEqualTo(COMPILATION_ERROR)
-    assertThat(result.messages)
-      .contains("value classes with default values are not currently supported in Moshi code gen")
-  }
-
-  @Test
   fun valueClassesCannotHaveDefaultValues() {
     val result =
       compile(
