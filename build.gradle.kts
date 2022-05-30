ee4b0e5e8fab3c1649303fb108ba70cd6d0d9b36
Zac Sweers <pandanomic@gmail.com>
2022-05-29 22:00:31 -0400

    Ktfmt cleanup (#275)
    * Move ktfmt versioning to toml
    
    * Fix version
    
    * Format

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,8 @@ apiValidation {
 
 repositories { mavenCentral() }
 
+val ktfmtVersion = libs.versions.ktfmt.get()
+
 spotless {
   format("misc") {
     target("*.md", ".gitignore")
@@ -67,7 +69,7 @@ spotless {
     targetExclude("**/spotless.java", "**/build/**")
   }
   kotlin {
-    ktfmt("0.37").googleStyle()
+    ktfmt(ktfmtVersion).googleStyle()
     target("**/*.kt")
     trimTrailingWhitespace()
     endWithNewline()
@@ -78,7 +80,7 @@ spotless {
     )
   }
   kotlinGradle {
-    ktfmt("0.37").googleStyle()
+    ktfmt(ktfmtVersion).googleStyle()
     target("**/*.gradle.kts")
     trimTrailingWhitespace()
     endWithNewline()
