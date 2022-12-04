688545605d29b8f67ecedda9430f36f949d92e2a
Zac Sweers <pandanomic@gmail.com>
2022-12-04 17:48:09 -0500

    Implement in java-sealed

--- moshi-sealed/java-sealed-reflect/src/main/java/dev/zacsweers/moshix/sealed/java/JavaSealedJsonAdapterFactory.java
+++ moshi-sealed/java-sealed-reflect/src/main/java/dev/zacsweers/moshix/sealed/java/JavaSealedJsonAdapterFactory.java
@@ -15,6 +15,8 @@
  */
 package dev.zacsweers.moshix.sealed.java;
 
+import static dev.zacsweers.moshix.sealed.runtime.internal.Util.fallbackAdapter;
+
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.JsonClass;
 import com.squareup.moshi.Moshi;
@@ -22,6 +24,7 @@ import com.squareup.moshi.Types;
 import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory;
 import dev.zacsweers.moshix.sealed.annotations.DefaultNull;
 import dev.zacsweers.moshix.sealed.annotations.DefaultObject;
+import dev.zacsweers.moshix.sealed.annotations.FallbackAdapter;
 import dev.zacsweers.moshix.sealed.annotations.TypeLabel;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
@@ -51,9 +54,21 @@ public final class JavaSealedJsonAdapterFactory implements JsonAdapter.Factory {
     if (labelKey == null) {
       return null;
     }
-    var defaultObject = UNSET;
+    var defaultValue = UNSET;
     if (rawType.isAnnotationPresent(DefaultNull.class)) {
-      defaultObject = null;
+      defaultValue = null;
+    }
+    JsonAdapter<Object> fallbackAdapter = null;
+    var fallbackAdapterAnnotation = rawType.getAnnotation(FallbackAdapter.class);
+    if (fallbackAdapterAnnotation != null) {
+      var clazz = fallbackAdapterAnnotation.value();
+      fallbackAdapter = fallbackAdapter(moshi, clazz);
+    }
+
+    if (defaultValue != UNSET && fallbackAdapter != null) {
+      throw new IllegalStateException(
+          "Only one of @DefaultNull or @FallbackAdapter can be used at a time. Found both on "
+              + rawType);
     }
 
     var labels = new LinkedHashMap<String, Class<?>>();
@@ -73,8 +88,10 @@ public final class JavaSealedJsonAdapterFactory implements JsonAdapter.Factory {
     for (var entry : labels.entrySet()) {
       factory = factory.withSubtype(entry.getValue(), entry.getKey());
     }
-    if (defaultObject != UNSET) {
+    if (defaultValue != UNSET) {
       factory = factory.withDefaultValue(null);
+    } else if (fallbackAdapter != null) {
+      factory = factory.withFallbackJsonAdapter(fallbackAdapter);
     }
 
     return factory.create(rawType, annotations, moshi);
