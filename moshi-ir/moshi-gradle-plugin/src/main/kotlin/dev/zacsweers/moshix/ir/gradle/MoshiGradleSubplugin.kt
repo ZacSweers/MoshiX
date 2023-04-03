4dad2db7a332cdf91f13c64f4ee4eea52bbfb487
Zac Sweers <pandanomic@gmail.com>
2023-04-03 09:04:14 -0400

    Spotless + suppression

--- moshi-ir/moshi-gradle-plugin/src/main/kotlin/dev/zacsweers/moshix/ir/gradle/MoshiGradleSubplugin.kt
+++ moshi-ir/moshi-gradle-plugin/src/main/kotlin/dev/zacsweers/moshix/ir/gradle/MoshiGradleSubplugin.kt
@@ -1,18 +1,5 @@
-/*
- * Copyright (C) 2021 Zac Sweers
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2021 Zac Sweers
+// SPDX-License-Identifier: Apache-2.0
 package dev.zacsweers.moshix.ir.gradle
 
 import dev.zacsweers.moshi.ir.gradle.VERSION
@@ -25,7 +12,6 @@ import org.gradle.api.tasks.TaskProvider
 import org.gradle.language.jvm.tasks.ProcessResources
 import org.jetbrains.kotlin.gradle.plugin.FilesSubpluginOption
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilationWithResources
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
 import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
 import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
@@ -82,8 +68,10 @@ class MoshiGradleSubplugin : KotlinCompilerPluginSupportPlugin {
       val resourceOutputDir = getMoshiXResourceOutputDir(project, sourceSetName)
       val compilationTask = kotlinCompilation.compileTaskProvider
       compilationTask.configure { it.outputs.dirs(resourceOutputDir) }
+      @Suppress("DEPRECATION")
       val processResourcesTaskName =
-        (kotlinCompilation as? KotlinCompilationWithResources)?.processResourcesTaskName
+        (kotlinCompilation as? org.jetbrains.kotlin.gradle.plugin.KotlinCompilationWithResources)
+          ?.processResourcesTaskName
           ?: "processResources"
       project.locateTask<ProcessResources>(processResourcesTaskName)?.let { provider ->
         provider.configure { resourcesTask ->
