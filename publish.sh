90a87391c96e87bc4ba626d16d5db9eabda9ed4d
Zac Sweers <pandanomic@gmail.com>
2023-07-20 15:05:50 -0400

    Fix publish path

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ if [[ "$1" = "--local" ]]; then local=true; fi
 if ! [[ ${local} ]]; then
   cd moshi-ir/moshi-gradle-plugin publish
   ./gradlew publish -x dokkaHtml
-  cd ..
+  cd ../..
   ./gradlew publish -x dokkaHtml
 else
   ./gradlew -p moshi-ir/moshi-gradle-plugin publishToMavenLocal -x dokkaHtml
