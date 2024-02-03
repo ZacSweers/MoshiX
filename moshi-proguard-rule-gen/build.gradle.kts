399b895f10d54cdb04854b4e5874658ee6560b81
Zac Sweers <pandanomic@gmail.com>
2024-02-03 15:46:24 -0500

    Spotless

--- moshi-proguard-rule-gen/build.gradle.kts
+++ moshi-proguard-rule-gen/build.gradle.kts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   alias(libs.plugins.kotlinJvm)
