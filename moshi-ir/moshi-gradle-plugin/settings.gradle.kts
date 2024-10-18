4e0f2ad54eb94271a4a8cc242820dd398aadf799
Zac Sweers <pandanomic@gmail.com>
2024-10-18 16:58:19 -0400

    Update to Dokka 2

--- moshi-ir/moshi-gradle-plugin/settings.gradle.kts
+++ moshi-ir/moshi-gradle-plugin/settings.gradle.kts
@@ -29,3 +29,6 @@ dependencyResolutionManagement {
     google()
   }
 }
+
+// https://docs.gradle.org/current/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
