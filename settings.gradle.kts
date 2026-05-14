ce8a5634d3fb353b6d6f47af8dc849dcbc8f3eb4
Zac Sweers <pandanomic@gmail.com>
2026-05-14 16:56:17 -0400

    RC

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,7 +31,7 @@ pluginManagement {
     gradlePluginPortal()
   }
   plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.4.0-Beta2"
+    id("org.jetbrains.kotlin.jvm") version "2.4.0-RC"
     id("com.autonomousapps.build-health") version "3.6.1"
   }
 }
