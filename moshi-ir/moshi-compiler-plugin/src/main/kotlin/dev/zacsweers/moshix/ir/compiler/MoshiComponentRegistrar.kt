26b7e17e33b8f19c6f3b31098f8fd80a21fc2a01
Zac Sweers <pandanomic@gmail.com>
2026-07-01 17:39:33 -0400

    Register it

--- moshi-ir/moshi-compiler-plugin/src/main/kotlin/dev/zacsweers/moshix/ir/compiler/MoshiComponentRegistrar.kt
+++ moshi-ir/moshi-compiler-plugin/src/main/kotlin/dev/zacsweers/moshix/ir/compiler/MoshiComponentRegistrar.kt
@@ -3,9 +3,13 @@
 package dev.zacsweers.moshix.ir.compiler
 
 import com.google.auto.service.AutoService
+import dev.zacsweers.moshix.ir.compiler.fir.MoshiFirExtensionRegistrar
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+import org.jetbrains.kotlin.compiler.plugin.registerExtension
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 import org.jetbrains.kotlin.name.ClassId
 
 @AutoService(CompilerPluginRegistrar::class)
@@ -15,12 +19,14 @@ public class MoshiComponentRegistrar : CompilerPluginRegistrar() {
 
   override val supportsK2: Boolean = true
 
+  @OptIn(ExperimentalCompilerApi::class)
   override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
     if (configuration[KEY_ENABLED] == false) return
     val enableSealed = configuration[KEY_ENABLE_SEALED] == true
 
     val fqGeneratedAnnotation = configuration[KEY_GENERATED_ANNOTATION]?.let(ClassId::fromString)
 
+    FirExtensionRegistrar.registerExtension(MoshiFirExtensionRegistrar(enableSealed))
     IrGenerationExtension.registerExtension(
       MoshiIrGenerationExtension(fqGeneratedAnnotation, enableSealed)
     )
